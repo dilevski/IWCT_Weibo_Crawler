fd9234d3ce43d09456aff3e95f1705ffef8c4ef5
dilevski <tanzhaowei@tanmatoMacBook-Pro.local>
2014-05-13 13:09:48 +0800

    no use...

--- login/loginByCookie.py
+++ login/loginByCookie.py
@@ -4,7 +4,7 @@ import urllib2, urllib, cookielib, string, json, time
 
 def main():
     req = urllib2.Request(url = "http://weibo.com/")
-    req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.802.30 Safari/535.1 SE 2.X MetaSr 1.0')#添加浏览器信息（可以无视）
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.802.30 Safari/535.1 SE 2.X MetaSr 1.0')
     cookie = ""#put your cookie here
     req.add_header('Cookie', cookie)
     content = urllib2.urlopen(req).read()
